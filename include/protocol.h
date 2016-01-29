6673dd769a775edffd2387f57dc2472c1f8b1b3b
repstosw <repstosw1@gmail.com>
2016-01-29 20:10:32 +0000

    Add payload indices

--- include/protocol.h
+++ include/protocol.h
@@ -2,8 +2,20 @@
 
 #include <stdint.h>
 
+// Start and end bytes
 #define STX         0x02
 #define ETX         0x03
+
+// Buffer indices
+#define STX_IDX         0
+#define LEN_LSB_IDX     1
+#define LEN_MSB_IDX     2
+#define PAYLOAD_IDX     3
+// Indices based upon the payload length
+#define LRC_IDX(x)      x + 3
+#define CHKSUM_IDX(x)   x + 4
+#define ETX_IDX         x + 5
+
 
 // Get LSB and MSB from a 16-bit word
 #define LSB(x)  x & 0xFF
